6411d9aabd47f0bbfb405ad2bf1989e2d6be0c7e
anasvakyathodi <anasvakyathodi@gmail.com>
2020-08-03 22:12:53 +0530

    Updates

---
+++ precache-manifest.9af2c4fb2405059ccea565602ba9cd7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff3400c3f246dbd16ecb9b9198433574",
+    "url": "/material-ui/index.html"
+  },
+  {
+    "revision": "4a9cfc0785d89139a156",
+    "url": "/material-ui/static/js/2.f6bccfd2.chunk.js"
+  },
+  {
+    "revision": "b47b83f293ef0a67523532a1d2a28ee0",
+    "url": "/material-ui/static/js/2.f6bccfd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d101c9450cf429262a5",
+    "url": "/material-ui/static/js/main.c39338f9.chunk.js"
+  },
+  {
+    "revision": "30b578e52fd2949f04f5",
+    "url": "/material-ui/static/js/runtime-main.1b5b7e7d.js"
+  }
+]);
